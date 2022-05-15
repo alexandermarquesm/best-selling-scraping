5ced7f2932e0dd79b8bbd0f554beae70540bd699
alexandermarquesm <alexandermarquesoficial@gmail.com>
2022-05-15 15:22:08 -0300

    feat(scraping-artists.py): verificando se estamos recebendo os dados

--- scraping-artists.py
+++ scraping-artists.py
@@ -1,3 +1,8 @@
-import bs4 as bs
+from bs4 import BeautifulSoup
 import pandas as pd
 import requests
+
+url = 'https://en.wikipedia.org/wiki/List_of_best-selling_music_artists'
+
+response = requests.get(url)
+print(response.status_code == 200)
